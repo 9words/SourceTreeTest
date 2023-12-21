79958bda147c92e96a828df2e3d1ffa4d670b3b4
13189803173@163.com <13189803173@163.com>
2023-12-21 19:31:30 +0800

    创建trigger

---
+++ sourceTreeTest/Assets/Script/TriggerChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if(col.gameObject.tag=="Ball")
+        {
+            Invoke("Falldown", 0.5f);
+        }
+    }
+    void Falldown()
+    {
+       GetComponentInParent<Rigidbody>().useGravity = true;
+       // Destroy(transform.gameObject);
+        Destroy(transform.parent.gameObject, 2f);
+    }
+}
