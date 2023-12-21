796304ba297c0cdf1580fa9b5db5bd65d386078e
13189803173@163.com <13189803173@163.com>
2023-12-21 20:12:13 +0800

    随机生成平台

---
+++ sourceTreeTest/Assets/Script/PlaneformSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneformSpawner : MonoBehaviour
+{
+    public GameObject planeform;
+    Vector3 lastpos;
+    float size;
+   public bool gameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastpos = transform.position;
+        size = planeform.transform.localScale.x;
+        for(int i=0;i<20;i++)
+        {
+            Randominstan();
+        }
+        InvokeRepeating("Randominstan",2f,0.2f);
+    }
+    void Randominstan()
+    {
+        
+        float rand=Random.Range(0, 6);
+        if(rand<3)
+        {
+            SpawnX();
+        }
+        else if(rand>=3)
+         {
+            SpawnY();
+        }
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameover)
+        {
+            CancelInvoke("Randominstan");
+        }
+    }
+    void SpawnX()
+    {
+        Vector3 temppos = lastpos;
+        temppos.x += size;
+        lastpos = temppos;
+
+        Instantiate(planeform, lastpos, Quaternion.identity);
+    }
+    void SpawnY()
+    {
+        Vector3 temppos = lastpos;
+        temppos.z += size;
+        lastpos = temppos;
+
+        Instantiate(planeform, lastpos, Quaternion.identity);
+    }
+}
