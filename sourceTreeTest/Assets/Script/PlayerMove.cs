d22f7b2181ab541aa1bde905989cd9ef53386ab6
13189803173@163.com <13189803173@163.com>
2023-12-21 12:54:45 +0800

    增加了playerMove的脚本和plyaer移动简易场景

---
+++ sourceTreeTest/Assets/Script/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xinput = Input.GetAxis("Horizontal")*speed;
+        float yinput = Input.GetAxis("Vertical")*speed;
+
+       rb.velocity = new Vector3(xinput,0,yinput);
+    }
+}
